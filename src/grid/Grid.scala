5fcc8c3209760d6d86f9036f5606a2c8ed8ff899
mattfowler <matt.fow@gmail.com>
2013-02-12 00:27:50 -0500

    refactor using tuples and recursion so that i don't have 8 calls to get neighbor and I don't have to nastily filter out nulls (yuck).

--- src/grid/Grid.scala
+++ src/grid/Grid.scala
@@ -2,37 +2,38 @@ package grid
 
 import cell.{LiveCell, Cell, DeadCell}
 
-final class Grid(private val dimension:Int) {
+final class Grid(private val dimension: Int) {
 
-  private var grid:Array[Array[Cell]] = createGrid(dimension)
+  private var grid: Array[Array[Cell]] = createGrid(dimension)
 
-  def getCurrentGeneration:Array[Array[Cell]] = {
+  def getCurrentGeneration: Array[Array[Cell]] = {
     return grid;
   }
 
-  def createGrid(dimension:Int):Array[Array[Cell]] = {
+  def createGrid(dimension: Int): Array[Array[Cell]] = {
     grid = Array.fill(dimension, dimension)(new DeadCell(List()))
     for (i <- 0 to dimension - 1) {
       for (j <- 0 to dimension - 1) {
-        val neighbors = List(getNeighbor(i-1, j-1),
-                             getNeighbor(i+1, j+1),
-                             getNeighbor(i+1, j-1),
-                             getNeighbor(i-1, j+1),
-                             getNeighbor(i-1, j),
-                             getNeighbor(i  , j-1),
-                             getNeighbor(i+1, j),
-                             getNeighbor(i  , j+1))
-        grid(i)(j).neighbors = neighbors.filterNot(_ == null)
+        val neighbors = List((i-1, j-1), (i+1, j+1), (i+1, j-1), (i-1, j+1),
+                             (i-1, j)  , (i  , j-1), (i+1, j)  , (i  , j+1))
+        grid(i)(j).neighbors = neighborList(neighbors)
       }
     }
     return grid;
   }
 
-  private def getNeighbor(row:Int, column:Int):Cell = {
-    if (row < 0 || column < 0 || row > grid.length-1 || column > grid.length-1) {
-      return null
+  private def neighborList(indices: List[(Int, Int)]): List[Cell] = {
+    if (indices.isEmpty) {
+      return Nil;
     } else {
-      return grid(row)(column)
+      val rowAndCol = indices.head
+      val row = rowAndCol._1
+      val col = rowAndCol._2
+      if (row < 0 || col < 0 || row > grid.length - 1 || col > grid.length - 1) {
+        return neighborList(indices.drop(1))
+      } else {
+        return grid(row)(col) :: neighborList(indices.drop(1))
+      }
     }
   }
 }
